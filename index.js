4dda277f1de72cc16b00c7d63c4106f6a1cec80f
CutSewTech <sales.cutsewtech@gmail.com>
2022-12-25 13:26:44 +0600

    add env

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 
 
-const uri = "mongodb+srv://CutSewTech:CmLYStHT4N!XgiU@cluster0.dzdxbuj.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dzdxbuj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
